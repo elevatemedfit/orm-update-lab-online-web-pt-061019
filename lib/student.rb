2c77a462dd3592e0ad8c46a2fee29edf31260077
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 17:37:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,12 +23,12 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
-
+      LIMIT 1
       SQL
 
     DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
-      end
+    end.first
     end
 
   def self.create(name, grade)
