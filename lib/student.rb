6d24cf9e4c9d4c2ff4acfe386dc3586c52aea074
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 17:43:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,12 +11,18 @@ class Student
     @grade = grade
   end
 
+  # def self.new_from_db(row)
+  #   student = self.new
+  #   student.id = row[0]
+  #   student.name = row[1]
+  #   student.grade = row[2]
+  #   student
+  # end
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
