095392d0319f5c72c1c0b9edf8d822db13335024
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 17:02:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,15 @@ class Student
     @grade = grade
   end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL 
+    DB[:conn].execute(sql)
+  end
 
 end
