9a74dc17a837be9645b806df87dc685d961a0e10
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 17:35:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,19 @@ class Student
     student
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+
+      SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+      end.first
+    end
+
   def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
