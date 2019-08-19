3fca6a290777721a28c90170ef81b1d54ee305e3
Chris Junker <elevatemedfit@gmail.com>
2019-08-19 17:58:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,8 @@ class Student
     end.first
     end
 
-  def self.create(name:, grade:)
-    student = Student.new(name:, grade:)
+  def self.create(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end
